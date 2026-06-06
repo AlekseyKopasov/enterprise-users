2ba1481d4008bfe58995671968d41ec382eef10f
Aleksey.Kopasov <alesey.k1@yandex.ru>
2026-06-06 08:56:25 +0300

    initial commit

---
+++ src/entities/user/types.ts
@@ -0,0 +1,14 @@
+type UserDto  = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  email: string,
+  role: string
+};
+
+type User = {
+  id: number;
+  fullName: string;
+  email: string;
+  role: string;
+};
